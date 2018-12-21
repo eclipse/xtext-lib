bf8e2c40baed94f59928e09a402ae67d25ea3d59
Karsten Thoms <karsten.thoms@itemis.de>
2018-12-21 13:48:39 +0100

    [eclipse/xtext#1249] BOM management
    - Add initial step to install the BOM into the local maven repository
    - Use local maven repository in the Gradle build to resolve the
    previously built BOM
    - Use Spring dependency-management plugin
    - Remove some managed versions
    - add convenience shell scripts for local builds
    - Switched Jenkinsfile to Declarative Pipeline
    
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

---
+++ 3-maven-build.sh
@@ -0,0 +1,8 @@
+mvn \
+  -f releng \
+  --batch-mode \
+  --update-snapshots \
+  -Dmaven.repo.local=.m2/repository \
+  -DJENKINS_URL=$JENKINS_URL \
+  -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn \
+  clean install
