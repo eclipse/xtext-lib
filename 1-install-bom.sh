9e0be8272f8bdb92db50a0691517a1e5177296fa
Sebastian Zarnekow <sebastian.zarnekow@gmail.com>
2019-01-09 14:37:50 +0100

    Use env vars

--- 1-install-bom.sh
+++ 1-install-bom.sh
@@ -3,6 +3,6 @@ mvn \
   --batch-mode \
   --update-snapshots \
   -Dmaven.repo.local=.m2/repository \
-  -DJENKINS_URL=https://ci.eclipse.org/xtext/ \
+  -DJENKINS_URL=$JENKINS_URL \
   -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn \
   clean install
--- 2-gradle-build.sh
+++ 2-gradle-build.sh
@@ -2,7 +2,7 @@
   clean cleanGenerateXtext build createLocalMavenRepo \
   -Dmaven.repo.local=$(pwd)/.m2/repository \
   -PcompileXtend=true \
-  -PJENKINS_URL=https://ci.eclipse.org/xtext/ \
+  -PJENKINS_URL=$JENKINS_URL \
   -PignoreTestFailures=true \
   --refresh-dependencies \
   --continue
